93dbfd5b0f8001eff8f08c525a2131d77b33c01f
dymaxionkim <dymaxion.kim@gmail.com>
2021-12-12 20:45:05 +0900

    Pretify Code

--- FGPG2.py
+++ FGPG2.py
@@ -24,6 +24,8 @@ THETA3_MIN, Start Angle of Edge Round Curve
 THETA3_MAX, End Angle of Edge Round Curve
 ALPHA_TS, Start Angle of Root Round Curve
 THETA_TE, End Angle of Root Round Curve
+P_ANGLE, Gear Pitch Angle
+ALIGN_ANGLE, Gear Align Angle
 
 # Linspaces
 THETA1, Linspace for Involute curve
@@ -52,7 +54,9 @@ def Parameters(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,
     # modify "E"
     if (ALPHA_E>ALPHA_M) and (ALPHA_M>ALPHA_IS+THETA_IE-np.arctan(THETA_IE)) :
         E = (E/2)*np.cos(ALPHA_0)*(THETA_IE-np.sqrt((1/np.cos(ALPHA_IS+THETA_IE-ALPHA_M))**2-1))
-    return ALPHA_0,ALPHA_M,ALPHA_IS,THETA_IS,THETA_IE,ALPHA_E,X_E,Y_E,X_E0,Y_E0,ALPHA_TS,THETA_TE,E
+    P_ANGLE = 2*np.pi/Z
+    ALIGN_ANGLE = np.pi/2-np.pi/Z
+    return ALPHA_0,ALPHA_M,ALPHA_IS,THETA_IS,THETA_IE,ALPHA_E,X_E,Y_E,X_E0,Y_E0,ALPHA_TS,THETA_TE,E,P_ANGLE,ALIGN_ANGLE
 
 def SymetryY(XX,YY):
     XX2 =  XX[::-1]
@@ -97,29 +101,7 @@ def RootArc(M,Z,X,D,ALPHA_TS,SEG_ROOT):
     Y51 = M*(Z/2-D+X)*np.sin(THETA7)
     return X51,Y51,THETA7
 
-
-def FGPG2_PLOT(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,SEG_ROOT_R,SEG_OUTER,SEG_ROOT,SCALE):
-    # Set Graphics
-    fig = plt.figure(figsize=(5,5))
-    plt.axes().set_aspect('equal')
-    plt.title('Fine Gear Profile Generator 2')
-    plt.grid(True)
-    
-    # Gear tooth
-    ALPHA_0,ALPHA_M,ALPHA_IS,THETA_IS,THETA_IE,ALPHA_E,X_E,Y_E,X_E0,Y_E0,ALPHA_TS,THETA_TE,E = Parameters(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,SEG_ROOT_R,SEG_OUTER,SEG_ROOT,SCALE)
-    X11,Y11,THETA1 = InvoluteCurve(M,Z,SEG_INVOLUTE,THETA_IS,THETA_IE,ALPHA_0,ALPHA_IS)
-    X12,Y12 = SymetryY(X11,Y11)
-    X21,Y21,THETA3,THETA3_MIN,THETA3_MAX = EdgeRoundCurve(M,E,X11,Y11,X_E,Y_E,X_E0,Y_E0,SEG_EDGE_R)
-    X22,Y22 = SymetryY(X21,Y21)
-    X31,Y31,THETA_T,THETA_S = RootRoundCurve(M,Z,X,D,C,B,THETA_TE,ALPHA_TS,SEG_ROOT_R)
-    X32,Y32 = SymetryY(X31,Y31)
-    X41,Y41,THETA6 = OuterArc(M,Z,X,A,ALPHA_E,ALPHA_M,SEG_OUTER)
-    X42,Y42 = SymetryY(X41,Y41)
-    X51,Y51,THETA7 = RootArc(M,Z,X,D,ALPHA_TS,SEG_ROOT)
-    X52,Y52 = SymetryY(X51,Y51)
-
-    # Plot Whole Gear
-    p_angle = 2*np.pi/z
+def CombineTooth(X11,Y11,X21,Y21,X31,Y31,X41,Y41,X51,Y51,X12,Y12,X22,Y22,X32,Y32,X42,Y42,X52,Y52):
     # Combine to one tooth
     X23 = np.delete(X22,0)
     Y23 = np.delete(Y22,0)
@@ -135,50 +117,159 @@ def FGPG2_PLOT(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,
     Y24 = np.delete(Y21,0)
     X1 = np.concatenate((X42,X23,X13,X33,X52,X51,X34,X14,X24,X41))
     Y1 = np.concatenate((Y42,Y23,Y13,Y33,Y52,Y51,Y34,Y14,Y24,Y41))
-    # Align to top
-    align_angle = np.pi/2-np.pi/Z
-    X2 = np.cos(align_angle)*X1 - np.sin(align_angle)*Y1
-    Y2 = np.sin(align_angle)*X1 + np.cos(align_angle)*Y1
+    return X1,Y1
+
+def Transform(Xtemp,Ytemp,X_0,Y_0):
+    Xtemp = Xtemp + X_0
+    Ytemp = Ytemp + Y_0
+    return Xtemp,Ytemp
+
+def Rotation(Xtemp,Ytemp,ANGLE,i):
+    XX = np.cos(ANGLE*i)*Xtemp - np.sin(ANGLE*i)*Ytemp
+    YY = np.sin(ANGLE*i)*Xtemp + np.cos(ANGLE*i)*Ytemp
+    return XX,YY
+
+def Circle(DIA,SEG_CIRCLE):
+    THETA0 = np.linspace(0.0,2*np.pi,SEG_CIRCLE)
+    XX = DIA/2*np.sin(THETA0)
+    YY = DIA/2*np.cos(THETA0)
+    return XX,YY
+
+def CombineToothDXF(X12,Y12,X22,Y22,X32,Y32):
+    X13 = np.delete(X12,0)
+    Y13 = np.delete(Y12,0)
+    X33 = np.delete(X32,0)
+    Y33 = np.delete(Y32,0)
+    X1 = np.concatenate((X22,X13,X33))
+    Y1 = np.concatenate((Y22,Y13,Y33))
+    return X1,Y1
+
+def SaveDXF(X12,Y12,X22,Y22,X32,Y32,Z,X_0,Y_0,OUTER_DIA,ROOT_DIA,OFFSET_DIA):
+    doc = ezdxf.new('R2000')
+    msp = doc.modelspace()
+    # Spline Curve
+    Xdxf1,Ydxf1 = CombineToothDXF(X12,Y12,X22,Y22,X32,Y32)
+    ALIGN_ANGLE_DXF = np.pi/2+np.pi/Z
+    Xdxf3,Ydxf3 = Rotation(Xdxf1,Ydxf1,ALIGN_ANGLE_DXF,1) # Align to top
+    Xdxf4 = -Xdxf3
+    Ydxf4 =  Ydxf3
+    Xdxf5,Ydxf5 = Transform(Xdxf3,Ydxf3,X_0,Y_0)
+    Xdxf6,Ydxf6 = Transform(Xdxf4,Ydxf4,X_0,Y_0)
+    cpoint5 = [(Xdxf5[0],Ydxf5[0])]
+    cpoint6 = [(Xdxf6[0],Ydxf6[0])]
+    for i in range(0,len(Xdxf5)) :
+        cpoint5.append((Xdxf5[i],Ydxf5[i]))
+        cpoint6.append((Xdxf6[i],Ydxf6[i]))
+    msp.add_spline(cpoint5)
+    msp.add_spline(cpoint6)
+    # Arc outer
+    outer_angle = np.arctan(Xdxf3[0]/Ydxf3[0])*360.0/(2.0*np.pi)
+    msp.add_arc(center=(X_0,Y_0), radius=OUTER_DIA/2, start_angle=90+outer_angle, end_angle=90-outer_angle)
+    # Arc root
+    root_start_angle = 180/Z
+    root_end_angle = np.arctan(Xdxf3[len(Xdxf3)-1]/Ydxf3[len(Xdxf3)-1])*360.0/(2.0*np.pi)
+    msp.add_arc(center=(X_0,Y_0), radius=ROOT_DIA/2, start_angle=90-root_start_angle, end_angle=90+root_end_angle)
+    msp.add_arc(center=(X_0,Y_0), radius=ROOT_DIA/2, start_angle=90-root_end_angle, end_angle=90+root_start_angle)
+    # Line dividing
+    Xdivide1 = -np.sin(np.pi/Z)*ROOT_DIA/2 + X_0
+    Ydivide1 =  np.cos(np.pi/Z)*ROOT_DIA/2 + Y_0
+    Xdivide2 = -np.sin(-np.pi/Z)*ROOT_DIA/2 + X_0
+    Ydivide2 =  np.cos(-np.pi/Z)*ROOT_DIA/2 + Y_0
+    msp.add_line([X_0,Y_0],[Xdivide1,Ydivide1])
+    msp.add_line([X_0,Y_0],[Xdivide2,Ydivide2])
+    # Circle offset
+    msp.add_circle((X_0,Y_0),radius=OFFSET_DIA/2)
+    # Output
+    Result = os.path.join(values['-WorkingDirectoty-'], f'Result.dxf')
+    doc.saveas(Result)
+
+def SaveSpec(M,ALPHA,Z,X,A,D,B,BASE_DIA,PITCH_DIA,OFFSET_DIA,ROOT_DIA,OUTER_DIA):
+    Result3 = os.path.join(values['-WorkingDirectoty-'], f'Result.csv')
+    fileout = open(Result3, "w")
+    if ALPHA == 20 :
+        fileout.write("Type,"+"Standard,\n")
+    else :
+        fileout.write("Type,"+"Non-Standard,\n")
+    fileout.write("Module,"+repr(M)+",mm"+"\n")
+    fileout.write("Pressure Angle,"+repr(ALPHA)+",deg\n")
+    fileout.write("Teeth Number,"+repr(Z)+",ea\n")
+    fileout.write("Offset Factor,"+repr(X)+",\n")
+    fileout.write("Offset,"+repr(X*M)+",mm\n")
+    fileout.write("Backlash Factor,"+repr(B)+",\n")
+    fileout.write("Backlash,"+repr(B*M)+",mm\n")
+    fileout.write("Addendum Factor,"+repr(A)+",\n")
+    fileout.write("Addendum,"+repr(A*M)+",mm\n")
+    fileout.write("Dedendum Factor,"+repr(D)+",\n")
+    fileout.write("Dedendum,"+repr(D*M)+",mm\n")
+    fileout.write("Total Tooth Height,"+repr(A+D)+",mm\n")
+    fileout.write("Base Circle Dia,"+repr(BASE_DIA)+",mm\n")
+    fileout.write("Pitch Circle Dia,"+repr(PITCH_DIA)+",mm\n")
+    fileout.write("Offset Circle Dia,"+repr(OFFSET_DIA)+",mm\n")
+    fileout.write("Root Circle Dia,"+repr(ROOT_DIA)+",mm\n")
+    fileout.write("Outer Circle Dia,"+repr(OUTER_DIA)+",mm\n")
+    fileout.close()
+
+def FGPG2_PLOT(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,SEG_ROOT_R,SEG_OUTER,SEG_ROOT,SCALE):
+
+    ####################
+    # Set Graphics
+    fig = plt.figure(figsize=(5,5))
+    plt.axes().set_aspect('equal')
+    plt.title('Fine Gear Profile Generator 2')
+    plt.grid(True)
+    
+    # Gear tooth
+    ALPHA_0,ALPHA_M,ALPHA_IS,THETA_IS,THETA_IE,ALPHA_E,X_E,Y_E,X_E0,Y_E0,ALPHA_TS,THETA_TE,E,P_ANGLE,ALIGN_ANGLE = Parameters(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,SEG_ROOT_R,SEG_OUTER,SEG_ROOT,SCALE)
+    X11,Y11,THETA1 = InvoluteCurve(M,Z,SEG_INVOLUTE,THETA_IS,THETA_IE,ALPHA_0,ALPHA_IS)
+    X12,Y12 = SymetryY(X11,Y11)
+    X21,Y21,THETA3,THETA3_MIN,THETA3_MAX = EdgeRoundCurve(M,E,X11,Y11,X_E,Y_E,X_E0,Y_E0,SEG_EDGE_R)
+    X22,Y22 = SymetryY(X21,Y21)
+    X31,Y31,THETA_T,THETA_S = RootRoundCurve(M,Z,X,D,C,B,THETA_TE,ALPHA_TS,SEG_ROOT_R)
+    X32,Y32 = SymetryY(X31,Y31)
+    X41,Y41,THETA6 = OuterArc(M,Z,X,A,ALPHA_E,ALPHA_M,SEG_OUTER)
+    X42,Y42 = SymetryY(X41,Y41)
+    X51,Y51,THETA7 = RootArc(M,Z,X,D,ALPHA_TS,SEG_ROOT)
+    X52,Y52 = SymetryY(X51,Y51)
+
+    # Plot Whole Gear
+    X1,Y1 = CombineTooth(X11,Y11,X21,Y21,X31,Y31,X41,Y41,X51,Y51,X12,Y12,X22,Y22,X32,Y32,X42,Y42,X52,Y52)
+    ALIGN_ANGLE = np.pi/2-np.pi/Z
+    X2,Y2 = Rotation(X1,Y1,ALIGN_ANGLE,1) # Align to top
     # Array
-    for i in range(0,Z) :
-        # Rotate
-        Xtemp = np.cos(p_angle*i)*X2 - np.sin(p_angle*i)*Y2
-        Ytemp = np.sin(p_angle*i)*X2 + np.cos(p_angle*i)*Y2
-        # Othogonal Transformation
-        Xtemp = Xtemp + X_0
-        Ytemp = Ytemp + Y_0
-        # Plot
+    P_ANGLE = 2*np.pi/Z
+    for i in range(0,Z):
+        Xtemp,Ytemp = Rotation(X2,Y2,P_ANGLE,i)
+        Xtemp,Ytemp = Transform(Xtemp,Ytemp,X_0,Y_0)
         plt.plot(Xtemp,Ytemp,'-',linewidth=1.5,color='black',label='_nolegend_')
     
     # Base Cicle
-    THETA0 = np.linspace(0.0,2*np.pi,SEG_CIRCLE)
     base_dia = M*Z*np.cos(ALPHA_0)
-    X_base = base_dia/2*np.sin(THETA0) + X_0
-    Y_base = base_dia/2*np.cos(THETA0) + Y_0
+    X_base,Y_base = Circle(base_dia,SEG_CIRCLE)
+    X_base,Y_base = Transform(X_base,Y_base,X_0,Y_0)
     plt.plot(X_base, Y_base, ':', linewidth=1.0, color='cyan',label='Base Cicle')
     
     # Pitch Circle
     pitch_dia = M*Z
-    X_pitch = pitch_dia/2*np.sin(THETA0) + X_0
-    Y_pitch = pitch_dia/2*np.cos(THETA0) + Y_0
+    X_pitch,Y_pitch = Circle(pitch_dia,SEG_CIRCLE)
+    X_pitch,Y_pitch = Transform(X_pitch,Y_pitch,X_0,Y_0)
     plt.plot(X_pitch, Y_pitch, ':', linewidth=1.0, color='magenta',label='Pitch Circle')
 
     # Offset Circle
     offset_dia = 2*M*(Z/2+X)
-    X_offset = (offset_dia/2)*np.sin(THETA0) + X_0
-    Y_offset = (offset_dia/2)*np.cos(THETA0) + Y_0
+    X_offset,Y_offset = Circle(offset_dia,SEG_CIRCLE)
+    X_offset,Y_offset = Transform(X_offset,Y_offset,X_0,Y_0)
     plt.plot(X_offset, Y_offset, ':', linewidth=1.0, color='red',label='Offset Circle')
 
     # Outer Circle
     outer_dia = 2*M*(Z/2+X+A)
-    X_out = (outer_dia/2)*np.sin(THETA0) + X_0
-    Y_out = (outer_dia/2)*np.cos(THETA0) + Y_0
+    X_out,Y_out = Circle(outer_dia,SEG_CIRCLE)
+    X_out,Y_out = Transform(X_out,Y_out,X_0,Y_0)
     plt.plot(X_out, Y_out, ':', linewidth=1.0, color='brown',label='Outer Circle')
 
     # Root Circle
     root_dia = 2*M*(Z/2+X-D)
-    X_root = (root_dia/2)*np.sin(THETA0) + X_0
-    Y_root = (root_dia/2)*np.cos(THETA0) + Y_0
+    X_root,Y_root = Circle(root_dia,SEG_CIRCLE)
+    X_root,Y_root = Transform(X_root,Y_root,X_0,Y_0)
     plt.plot(X_root, Y_root, ':', linewidth=1.0, color='grey',label='Root Circle')
     
     # Annotate
@@ -215,14 +306,13 @@ def FGPG2_PLOT(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,
     plt.text(X_0,Y_0+Nrow/2-Cheight*14, 'Root Circle Dia = %s[mm]'%(root_dia),
         verticalalignment='center', horizontalalignment='center', color='grey', fontsize="x-small")
             
-    # Save Figure
-    #plt.legend(loc="lower left", ncol=1, fontsize="x-small")
-    # Total Gear
-    Result = os.path.join(WorkingDirectory, f'Result.png')
+    # Save Figure for Total Gear
+    Result = os.path.join(WorkingDirectory, f'Result1.png')
     plt.savefig(Result,dpi=100)
-    #Result = os.path.join(WorkingDirectory, f'Result.svg')
+    #Result = os.path.join(WorkingDirectory, f'Result1.svg')
     plt.savefig(Result)
-    # One Tooth
+
+    # Save Figure for One Tooth
     Y_height = ((outer_dia-root_dia)/2.0)/SCALE
     Y_cen = (outer_dia+root_dia)/4.0 + Y_0
     Y_min = Y_cen - Y_height/2.0
@@ -237,78 +327,12 @@ def FGPG2_PLOT(M,Z,ALPHA,X,B,A,D,C,E,X_0,Y_0,SEG_CIRCLE,SEG_INVOLUTE,SEG_EDGE_R,
     plt.savefig(Result2)
 
     ####################
-    # DXF
-    doc = ezdxf.new('R2000')
-    msp = doc.modelspace()
-    # Spline
-    Xdxf1 = np.concatenate((X22,X13,X33))
-    Ydxf1 = np.concatenate((Y22,Y13,Y33))
-    Xdxf3 = np.cos(np.pi/2+np.pi/Z)*Xdxf1 - np.sin(np.pi/2+np.pi/Z)*Ydxf1
-    Ydxf3 = np.sin(np.pi/2+np.pi/Z)*Xdxf1 + np.cos(np.pi/2+np.pi/Z)*Ydxf1
-    Xdxf4 = -Xdxf3
-    Ydxf4 =  Ydxf3
-    Xdxf5 = Xdxf3 + X_0
-    Ydxf5 = Ydxf3 + Y_0
-    Xdxf6 = Xdxf4 + X_0
-    Ydxf6 = Ydxf4 + Y_0
-    cpoint5 = [(Xdxf5[0],Ydxf5[0])]
-    cpoint6 = [(Xdxf6[0],Ydxf6[0])]
-    for i in range(0,len(Xdxf5)) :
-        cpoint5.append((Xdxf5[i],Ydxf5[i]))
-        cpoint6.append((Xdxf6[i],Ydxf6[i]))
-    msp.add_spline(cpoint5)
-    msp.add_spline(cpoint6)
-    
-    # Arc outer
-    outer_angle = np.arctan(Xdxf3[0]/Ydxf3[0])*360.0/(2.0*np.pi)
-    msp.add_arc(center=(X_0,Y_0), radius=outer_dia/2, start_angle=90+outer_angle, end_angle=90-outer_angle)
-    
-    # Arc root
-    root_start_angle = 180/Z
-    root_end_angle = np.arctan(Xdxf3[len(Xdxf3)-1]/Ydxf3[len(Xdxf3)-1])*360.0/(2.0*np.pi)
-    msp.add_arc(center=(X_0,Y_0), radius=root_dia/2, start_angle=90-root_start_angle, end_angle=90+root_end_angle)
-    msp.add_arc(center=(X_0,Y_0), radius=root_dia/2, start_angle=90-root_end_angle, end_angle=90+root_start_angle)
-    
-    # Line dividing
-    Xdivide1 = -np.sin(np.pi/Z)*root_dia/2 + X_0
-    Ydivide1 =  np.cos(np.pi/Z)*root_dia/2 + Y_0
-    Xdivide2 = -np.sin(-np.pi/Z)*root_dia/2 + X_0
-    Ydivide2 =  np.cos(-np.pi/Z)*root_dia/2 + Y_0
-    msp.add_line([X_0,Y_0],[Xdivide1,Ydivide1])
-    msp.add_line([X_0,Y_0],[Xdivide2,Ydivide2])
-    
-    # Circle offset
-    msp.add_circle((X_0,Y_0),radius=offset_dia/2)
-
-    Result = os.path.join(values['-WorkingDirectoty-'], f'Result.dxf')
-    doc.saveas(Result)
+    # Save DXF
+    SaveDXF(X12,Y12,X22,Y22,X32,Y32,Z,X_0,Y_0,outer_dia,root_dia,offset_dia)
     
     ####################
-    # Write Gear Spec
-    Result3 = os.path.join(values['-WorkingDirectoty-'], f'Result.csv')
-    fileout = open(Result3, "w")
-    if alpha == 20 :
-        fileout.write("Type,"+"Standard,\n")
-    else :
-        fileout.write("Type,"+"Non-Standard,\n")
-    fileout.write("Module,"+repr(M)+",mm"+"\n")
-    fileout.write("Pressure Angle,"+repr(ALPHA)+",deg\n")
-    fileout.write("Teeth Number,"+repr(Z)+",ea\n")
-    fileout.write("Offset Factor,"+repr(X)+",\n")
-    fileout.write("Offset,"+repr(X*M)+",mm\n")
-    fileout.write("Backlash Factor,"+repr(B)+",\n")
-    fileout.write("Backlash,"+repr(B*M)+",mm\n")
-    fileout.write("Addendum Factor,"+repr(A)+",\n")
-    fileout.write("Addendum,"+repr(A*M)+",mm\n")
-    fileout.write("Dedendum Factor,"+repr(D)+",\n")
-    fileout.write("Dedendum,"+repr(D*M)+",mm\n")
-    fileout.write("Total Tooth Height,"+repr(A+D)+",mm\n")
-    fileout.write("Base Circle Dia,"+repr(base_dia)+",mm\n")
-    fileout.write("Pitch Circle Dia,"+repr(pitch_dia)+",mm\n")
-    fileout.write("Offset Circle Dia,"+repr(offset_dia)+",mm\n")
-    fileout.write("Root Circle Dia,"+repr(root_dia)+",mm\n")
-    fileout.write("Outer Circle Dia,"+repr(outer_dia)+",mm\n")
-    fileout.close()    
+    # Save Gear Spec
+    SaveSpec(M,ALPHA,Z,X,A,D,B,base_dia,pitch_dia,offset_dia,root_dia,outer_dia)
 
 ##############################
 # GUI
@@ -347,7 +371,7 @@ right_col = [[sg.Text('Working Directory :',size=(8,1)), sg.Input('./Result/',ke
             [sg.Button('Load'), sg.Button('Run'), sg.Button('Toggle'), sg.Button('Exit')]]
 
 layout = [[sg.Column(left_col), sg.VSeperator(), sg.Column(right_col)]]
-window = sg.Window('FGPG2', layout)
+window = sg.Window('FGPG2', layout, icon="FGPG2.ico")
 
 while True:
     event, values = window.read()
@@ -387,7 +411,7 @@ while True:
     elif event == 'Run':
         os.makedirs(WorkingDirectory, exist_ok=True)
         FGPG2_PLOT(m,z,alpha,x,b,a,d,c,e,x_0,y_0,seg_circle,seg_involute,seg_edge_r,seg_root_r,seg_outer,seg_root,scale)
-        Result = os.path.join(WorkingDirectory, f'Result.png')
+        Result = os.path.join(WorkingDirectory, f'Result1.png')
         window['-IMAGE-'].update(Result,size=(500,500))
         Inputs = os.path.join(WorkingDirectory, f'Inputs.dat')
         window.save_to_disk(Inputs)
@@ -399,7 +423,7 @@ while True:
             window['-IMAGE-'].update(Result,size=(500,500))
             window['-TEXT-'].update('Toggle to One Tooth : OK')
         elif CurrentImage == 'Result2':
-            Result = os.path.join(WorkingDirectory, f'Result.png')
+            Result = os.path.join(WorkingDirectory, f'Result1.png')
             CurrentImage = 'Result'
             window['-IMAGE-'].update(Result,size=(500,500))
             window['-TEXT-'].update('Toggle to Whole Gear : OK')
