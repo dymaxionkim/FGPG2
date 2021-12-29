7ebabcda5dc3f43fe34c0a4a77d3d8d338701409
dymaxionkim <dymaxion.kim@gmail.com>
2021-12-29 23:18:22 +0900

    Add bat file

---
+++ FGPG2.bat
@@ -0,0 +1,11 @@
+REM Anaconda3 Environment
+set root=%USERPROFILE%\anaconda3
+call %root%\Scripts\activate.bat %root%
+call conda env list
+call conda activate base
+
+REM Start App
+call cd /d D:\git\FGPG2\
+call python FGPG2.py
+
+pause
