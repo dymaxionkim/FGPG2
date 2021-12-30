1f7899afa22dd8e1b9887df9a37e8601456fcda8
dhkima <dhkima@higenmotor.com>
2021-12-30 11:02:12 +0900

    Simplified Execute

--- FGPG2.bat
+++ FGPG2.bat
@@ -2,11 +2,12 @@ REM Anaconda3 Environment
 REM set root=%USERPROFILE%\anaconda3
 set root=%ProgramData%\anaconda3
 call %root%\Scripts\activate.bat %root%
-call conda env list
+REM call conda env list
 call conda activate base
 
 REM Start App
 call cd /d D:\git\FGPG2\
 call python FGPG2.py
 
-pause
+REM pause
+exit
