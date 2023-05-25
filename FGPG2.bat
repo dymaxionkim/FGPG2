a8c680ef818e41b8a581e38cd5c04fc7860928ee
dhkima-higenmotor <dhkima@higenmotor.com>
2023-05-25 17:02:24 +0900

    Change to scoop Miniconda3

--- FGPG2.bat
+++ FGPG2.bat
@@ -1,12 +1,14 @@
-REM Anaconda3 Environment
+REM Python Environment
 REM set root=%USERPROFILE%\anaconda3
-set root=%ProgramData%\anaconda3
+REM set root=%ProgramData%\anaconda3
+set root=%USERPROFILE%\scoop\apps\miniconda3\current
+
 call %root%\Scripts\activate.bat %root%
 REM call conda env list
 call conda activate base
 
 REM Start App
-call cd /d D:\git\FGPG2\
+call cd /d D:\github\FGPG2\
 call python FGPG2.py
 
 REM pause
